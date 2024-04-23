3427044b0e7d9bb88fc90781355ce20ad8f15d02
dericking01 <dericking01@gmail.com>
2024-04-23 14:13:10 +0000

    name change from Vetlink to Dodoki

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -10,7 +10,7 @@
       <div class="row justify-content-center g-0">
         <div class="col-lg-9 col-xl-8 col-xxl-6">
           <div class="card">
-            <div class="card-header bg-circle-shape bg-shape text-center p-2"><a class="font-sans-serif fw-bolder fs-4 z-1 position-relative link-light" href="#!" data-bs-theme="light">VETLINK - PORTAL</a></div>
+            <div class="card-header bg-circle-shape bg-shape text-center p-2"><a class="font-sans-serif fw-bolder fs-4 z-1 position-relative link-light" href="#!" data-bs-theme="light">DODOKI - PORTAL</a></div>
             <div class="card-body p-4">
               <div class="row flex-between-center">
                 <div class="col-auto">
