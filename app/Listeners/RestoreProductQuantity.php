87a303bcadd3f9e1244ddc677460908952a390d5
Baraka Elias <barakaurio0@gmail.com>
2025-04-25 02:15:27 +0300

    fix: wrap in transaction

--- app/Listeners/RestoreProductQuantity.php
+++ app/Listeners/RestoreProductQuantity.php
@@ -6,6 +6,8 @@ use App\Events\ProductQuantityRestored;
 use App\Models\ProductStock;
 use App\Models\AdminProduct;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
 
 class RestoreProductQuantity
 {
@@ -19,27 +21,36 @@ class RestoreProductQuantity
             $branchId = $order->branch_id;
             $productId = $orderItem->deductable_id;
 
-            // Find the product stock entry
-            $branchProduct = ProductStock::where('branch_id', $branchId)
+            try{
+                DB::beginTransaction();
+
+                // Find the product stock entry
+                $branchProduct = ProductStock::where('branch_id', $branchId)
                 ->where('admin_product_id', $productId)
                 ->first();
 
-            if ($branchProduct) {
-                $branchProduct->available_quantity += $orderItem->quantity;
-                $branchProduct->save();
-            } else {
-                // Log a warning if no stock is found for the product
-                Log::warning("Stock entry not found for branch_id: {$branchId}, admin_product_id: {$productId}");
-            }
+                if ($branchProduct) {
+                    $branchProduct->available_quantity += $orderItem->quantity;
+                    $branchProduct->save();
+                } else {
+                    // Log a warning if no stock is found for the product
+                    Log::warning("Stock entry not found for branch_id: {$branchId}, admin_product_id: {$productId}");
+                }
 
-             // Restore to AdminProduct (global stock)
-             $adminProduct = AdminProduct::find($productId);
-             if ($adminProduct) {
-                 $adminProduct->quantity += $quantity;
-                 $adminProduct->save();
-             } else {
-                 Log::warning("Admin product not found for ID: {$productId}");
-             }
+                // Restore to AdminProduct (global stock)
+                $adminProduct = AdminProduct::find($productId);
+                if ($adminProduct) {
+                    $adminProduct->quantity += $quantity;
+                    $adminProduct->save();
+                } else {
+                    Log::warning("Admin product not found for ID: {$productId}");
+                }
+
+                DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                Log::error('Failed to restore product quantity: ' . $e->getMessage());
+            }
         }
     }
 }
