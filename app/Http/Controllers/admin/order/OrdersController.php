20f5eb6e8df4ed71b67c0f0180b83ae1e8014868
Baraka Elias <barakaurio0@gmail.com>
2025-11-13 14:42:05 +0300

    fix: total_amount column name when creating an order, update price precision in branch_products

--- app/Http/Controllers/admin/order/OrdersController.php
+++ app/Http/Controllers/admin/order/OrdersController.php
@@ -294,7 +294,7 @@ class OrdersController extends Controller
         $order->payment_method = $request->payment_method;
         $order->sale_date = $request->filled('sale_date') ? $request->sale_date : Carbon::now()->toDateString();
         $order->isDelivered = false;
-        $order->amount = 0;
+        $order->total_amount = 0;
         // dd($order);
         $order->save();
 
